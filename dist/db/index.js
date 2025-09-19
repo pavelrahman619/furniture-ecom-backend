37e5f84723faca5d6a1f6813d72b8ca2d4146445
pavelrahman619 <pavelrahman.dev@gmail.com>
2025-09-19 19:43:14 +0600

    Remove dist directory from version control - now properly gitignored

--- dist/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect(`mongodb+srv://shongi:shongi1234@cluster0.mkvtpr5.mongodb.net/shongi?retryWrites=true&w=majority&appName=Cluster0`);
-console.log("Mongo DB Connection Successful ");
-exports.default = mongoose_1.default;
--- dist/lib/generateVapidKeys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const web_push_1 = __importDefault(require("web-push"));
-const vapidKeys = web_push_1.default.generateVAPIDKeys();
-console.log("Public Key:", vapidKeys.publicKey);
-console.log("Private Key:", vapidKeys.privateKey);
